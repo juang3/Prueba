d6d4c2bc48957a772e67437c1e9d09cda089ebd1
Administrador <Administrador@192.168.1.131>
2015-02-27 08:36:34 +0100

    Clase Prize Datos miembros privados: treasures y levels --> private int treasures, levels;

--- Napakalaki/src/napakalaki/Prize.java
+++ Napakalaki/src/napakalaki/Prize.java
@@ -10,5 +10,19 @@ package napakalaki;
  * @author Administrador
  */
 public class Prize {
+    private int treasures, levels;
+    
+    Prize(int tesoros, int niveles){
+        treasures   = tesoros;
+        levels      = niveles;
+    }
+    int getTreasures(){
+        return treasures;
+    }
+    int getLevels(){
+        return levels;
+    }
+    
+    
     
 }
